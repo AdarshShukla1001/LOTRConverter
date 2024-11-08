c8cc306298128e60254b0c1875de3eba8889efca
Adarsh Shukla <adarshshukla10012002@gmail.com>
2024-11-08 22:47:14 +0530

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by adarsh shukla on 08/11/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LOTRConverterTests/LOTRConverterTests.swift
@@ -0,0 +1,16 @@
+//
+//  LOTRConverterTests.swift
+//  LOTRConverterTests
+//
+//  Created by adarsh shukla on 08/11/24.
+//
+
+import Testing
+
+struct LOTRConverterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
