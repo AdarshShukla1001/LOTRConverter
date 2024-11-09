b554e1d99ab0a7a8c9679484ad7fbe26eff36803
Adarsh Shukla <adarshshukla10012002@gmail.com>
2024-11-09 10:29:39 +0530

    Added Design Content view

--- LOTRConverter/ContentView.swift
+++ LOTRConverter/ContentView.swift
@@ -9,13 +9,74 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            //            Background Image
+            Image(.background).resizable().ignoresSafeArea()
+            VStack {
+                //                Prainsing pony
+                Image(.prancingpony).resizable().scaledToFit().frame(height: 200)
+
+                //  currency exchange text
+                Text("Currency Exchange").font(.largeTitle).foregroundStyle(.white )
+
+                HStack {
+                    VStack {
+                        HStack {
+                            //   coin image
+                            Image(.silverpenny).resizable().scaledToFit().frame( height: 33)
+                            
+                            Text("Silver Peice")
+                                .font(.headline)
+                                .foregroundStyle(.white )
+
+                        }
+//                        Textfield
+                        Text("TextField")
+                    }
+                    
+                    Image(systemName: "equal").font(.largeTitle)
+                        .foregroundStyle(.white).symbolEffect(.pulse)
+                    //
+                    
+                    VStack {
+                        HStack {
+                            //   coin image
+                            
+                            
+                            Text("Gold Peice")
+                                .font(.headline)
+                                .foregroundStyle(.white )
+                            
+                            Image(.goldpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame( height: 33)
+
+                        }
+//                        Textfield
+                        Text("TextField")
+                        
+                        
+                        
+                    }
+                    
+                    
+                   
+                }
+                 
+                
+                HStack{
+                    Spacer()
+                    Image(systemName: "info.circle.fill")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white).padding(20)
+                }
+                //
+            }
+            .border(.blue)
+
+            //            Info Badge
         }
-        .padding()
     }
 }
 
